1df42df0dbad18986ee0d8dcf01a0f8f36b7ee22
Gowthami <gowthamihn8@gmail.com>
2026-04-18 15:56:09 +0530

    Update ThymeleafProjectApplication.java

--- src/main/java/com/jsp/task/ThymeleafProjectApplication.java
+++ src/main/java/com/jsp/task/ThymeleafProjectApplication.java
@@ -8,7 +8,7 @@ public class ThymeleafProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafProjectApplication.class, args);
-		System.out.println("Hello");
+		System.out.println("Helo");
 	}
 
 }
